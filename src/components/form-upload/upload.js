47337148ac53f699ff6baf750092260d0f5849a8
kharshita590 <83570759+kharshita590@users.noreply.github.com>
2024-10-18 17:24:50 +0530

    Update upload.js

--- src/components/form-upload/upload.js
+++ src/components/form-upload/upload.js
@@ -37,7 +37,7 @@ export default function FileUpload(){
 
             const queryString = `data=${encodeURIComponent(JSON.stringify(response.data.imgUrl))}`;  
             const objectId=response.data.idd
-            const url = `https://ar-converter-frontend.vercel.app/model/${objectId}`;  
+            const url = `https://ar-converter-backend-2.onrender.com/model/${objectId}`;  
             console.log(queryString);
             setUrl(url)
             setMsg("Upload completed");
