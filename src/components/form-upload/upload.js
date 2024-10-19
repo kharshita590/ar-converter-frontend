99746915a4ace8c56fb34e004c032e6d6e2864bd
harshitakumai-dev <harshitakumari344@gmail.com>
2024-10-19 12:48:26 +0530

    updated

--- src/components/form-upload/upload.js
+++ src/components/form-upload/upload.js
@@ -75,6 +75,9 @@ export default function FileUpload(){
     androidSource={img}
     iosSource="assets/models/ios/Salmon_tartine.usdz" 
     alternateName="Salmon Tartine" 
+    camera-controls 
+    ar
+    xr-environment
   />
      <a href={url} target="_blank" rel="noopener noreferrer" className="text-blue-500 underline">
         Go to Model
